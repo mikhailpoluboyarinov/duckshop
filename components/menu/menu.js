7f856861bfef6ddbe1713c4043b395715e8e6800
mikhailpoluboyarinov <mikhailpoluboyarinov@yandex.ru>
2025-03-31 16:28:24 +0300

    Поправил скролл на открытых попах и меню

--- components/menu/menu.js
+++ components/menu/menu.js
@@ -1,14 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
+    const body = document.body;
     const navItems = document.querySelectorAll(".nav__item");
     const burger = document.querySelector(".header__burger");
     const popupMenu = document.querySelector(".popup__menu");
     const closeButton = document.querySelector(".popup__menu__close");
     const callbackButton = document.querySelector(".popup__menu__callback");
     const popup = document.querySelector(".popup");
+    const popupCloseButton = document.querySelector(".popup__close-button");
 
     const resetActiveClass = () => {
         navItems.forEach(item => item.classList.remove("nav__item--active"));
-    }
+    };
 
     if (navItems.length > 0) {
         navItems[0].classList.add("nav__item--active");
@@ -21,15 +23,46 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 
-    burger.addEventListener("click", () => popupMenu.classList.add("active"));
-    closeButton.addEventListener("click", () => popupMenu.classList.remove("active"));
-    callbackButton.addEventListener("click", () => {
+    const openPopupMenu =() => {
+        popupMenu.classList.add("active");
+        body.classList.add("body_no-scroll");
+    }
+
+    const closePopupMenu = () => {
+        popupMenu.classList.remove("active");
+        body.classList.remove("body_no-scroll");
+    }
+
+    const openPopupForm =() => {
         popupMenu.classList.remove("active");
         popup.classList.add("popup--open");
+        body.classList.add("body_no-scroll");
 
         const firstInput = popup.querySelector(".form__input");
         if (firstInput) {
             setTimeout(() => firstInput.focus(), 100);
+        }
+    }
+
+    const closePopupForm =() => {
+        popup.classList.remove("popup--open");
+        body.classList.remove("body_no-scroll");
+    }
+
+    burger.addEventListener("click", openPopupMenu);
+    closeButton.addEventListener("click", closePopupMenu);
+    callbackButton.addEventListener("click", openPopupForm);
+    popupCloseButton.addEventListener("click", closePopupForm);
+
+    popup.addEventListener("click", (event) => {
+        if (event.target === popup) {
+            closePopupForm();
+        }
+    });
+
+    popupMenu.addEventListener("click", (event) => {
+        if (event.target === popupMenu) {
+            closePopupMenu();
         }
     });
 });
--- components/popup/popup.js
+++ components/popup/popup.js
@@ -2,17 +2,21 @@ document.addEventListener("DOMContentLoaded", () => {
     const popup = document.querySelector(".popup");
     const openPopupButton = document.querySelector(".header__callback");
     const closePopupButton = document.querySelector(".popup__close-button");
-    const form = document.querySelector(".form"); // Находим форму
-    const firstInput = form.querySelector(".form__input"); // Первое поле ввода
+    const form = document.querySelector(".form");
+    const firstInput = form.querySelector(".form__input");
 
     openPopupButton.addEventListener("click", () => {
+        document.body.classList.add("body_no-scroll");
         popup.classList.add("popup--open");
+
         if (firstInput) {
             setTimeout(() => firstInput.focus(), 100);
         }
     });
     const closePopup = () => {
+        document.body.classList.remove("body_no-scroll");
         popup.classList.remove("popup--open");
+
     }
 
     closePopupButton.addEventListener("click", closePopup);
